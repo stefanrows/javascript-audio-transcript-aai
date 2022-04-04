bfc0b0479fe0c5d7c6e875998b4b6a9f1896f508
Stefan Rows <38349705+stefanrows@users.noreply.github.com>
2022-04-04 14:56:14 +0200

    Final Update

---
+++ audio.js
@@ -0,0 +1,38 @@
+const axios = require("axios")
+const audioURL = "https://bit.ly/3yxKEIY"
+const APIKey = "YOUR-ASSEMBLYAI-API-KEY"
+const refreshInterval = 5000
+
+// Setting up the AssemblyAI headers
+const assembly = axios.create({
+  baseURL: "https://api.assemblyai.com/v2",
+  headers: {
+    authorization: APIKey,
+    "content-type": "application/json",
+  },
+})
+
+const getTranscript = async () => {
+  // Sends the audio file to AssemblyAI for transcription
+  const response = await assembly.post("/transcript", {
+    audio_url: audioURL,
+  })
+
+  // Interval for checking transcript completion
+  const checkCompletionInterval = setInterval(async () => {
+    const transcript = await assembly.get(`/transcript/${response.data.id}`)
+    const transcriptStatus = transcript.data.status
+    let transcriptText = ""
+
+    if (transcriptStatus !== "completed") {
+      console.log(`Transcript Status: ${transcriptStatus}`)
+    } else if (transcriptStatus === "completed") {
+      console.log("\nTranscription completed!\n")
+      transcriptText = transcript.data.text
+      console.log(`Your transcribed text:\n\n${transcriptText}`)
+      clearInterval(checkCompletionInterval)
+    }
+  }, refreshInterval)
+}
+
+getTranscript()
